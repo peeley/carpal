4f791f93a290062b52f11c3f5181d222efed6e42
Noah Snelson <noah.snelson@protonmail.com>
2023-08-12 12:26:42 -0700

    Add config file test for reading from fs

--- internal/config/main_test.go
+++ internal/config/main_test.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -29,6 +31,43 @@ file:
 
 		if !cmp.Equal(got, want) {
 			t.Errorf("got: %+v, want: %+v",	got, want)
+		}
+	})
+}
+
+func TestConfigWizardGetConfiguration(t *testing.T) {
+	t.Run("config wizard can read config file and parse configuration", func(t *testing.T) {
+		wizard := NewConfigWizard("../../test/config.yml")
+
+		got, err := wizard.GetConfiguration()
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		want := Configuration{
+			Driver: "file",
+			FileConfiguration: &FileConfiguration{
+				Directory: "./test/",
+			},
+		}
+
+		if cmp.Equal(got, want) {
+			t.Fatalf("got: %+v, want: %+v", got, want)
+		}
+	})
+
+	t.Run("config wizard errors on nonexistent config file", func(t *testing.T) {
+		wizard := NewConfigWizard("missingno")
+
+		got, err := wizard.GetConfiguration()
+
+		if err == nil {
+			t.Fatalf("expected wizard to error on missing file, got %+v", got)
+		}
+
+		if !errors.Is(err, os.ErrNotExist) {
+			t.Fatalf("error should be NotExist: %+v", err)
 		}
 	})
 }
