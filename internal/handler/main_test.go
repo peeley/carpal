ed6f071cd4fab0ceaeb3d3f55c06cb8fa44f3189
Noah Snelson <noah.snelson@protonmail.com>
2025-08-14 22:43:59 -0700

    Add Link filtering via the `rel` query parameter

--- internal/handler/main_test.go
+++ internal/handler/main_test.go
@@ -54,6 +54,72 @@ func TestResourceHandlerFileDriver(t *testing.T) {
 		}
 	})
 
+	t.Run("can filter links on resources via the `rel` query parameter", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "/", nil)
+		query := req.URL.Query()
+		query.Add("resource", "acct:bob@foobar.com")
+		query.Add("rel", "http://webfinger.example/rel/profile-page")
+		req.URL.RawQuery = query.Encode()
+
+		responseRecorder := httptest.NewRecorder()
+		httpHandler.ServeHTTP(responseRecorder, req)
+
+		if responseRecorder.Code != http.StatusOK {
+			t.Fatalf("expected 200 OK, got %v", responseRecorder.Code)
+		}
+
+		responseHeaders := responseRecorder.HeaderMap
+
+		if len(responseHeaders["Content-Type"]) != 1 ||
+			responseHeaders["Content-Type"][0] != "application/jrd+json" {
+			t.Fatalf(
+				"expected application/jrd+json content type, got %v",
+				responseHeaders["Content-Type"],
+			)
+		}
+
+		body := responseRecorder.Body.String()
+
+		want := `{"subject":"acct:bob@foobar.com","aliases":["mailto:bob@foobar.com","https://mastodon/bob"],"properties":{"http://webfinger.example/ns/name":"Bob Smith"},"links":[{"rel":"http://webfinger.example/rel/profile-page","href":"https://www.example.com/~bob/"}]}`
+
+		if body != want {
+			t.Fatalf("got: %+v,\n want: %+v", body, want)
+		}
+	})
+
+	t.Run("blank `rel` query param results in omitted links field", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "/", nil)
+		query := req.URL.Query()
+		query.Add("resource", "acct:bob@foobar.com")
+		query.Add("rel", "")
+		req.URL.RawQuery = query.Encode()
+
+		responseRecorder := httptest.NewRecorder()
+		httpHandler.ServeHTTP(responseRecorder, req)
+
+		if responseRecorder.Code != http.StatusOK {
+			t.Fatalf("expected 200 OK, got %v", responseRecorder.Code)
+		}
+
+		responseHeaders := responseRecorder.HeaderMap
+
+		if len(responseHeaders["Content-Type"]) != 1 ||
+			responseHeaders["Content-Type"][0] != "application/jrd+json" {
+			t.Fatalf(
+				"expected application/jrd+json content type, got %v",
+				responseHeaders["Content-Type"],
+			)
+		}
+
+		body := responseRecorder.Body.String()
+
+		want := `{"subject":"acct:bob@foobar.com","aliases":["mailto:bob@foobar.com","https://mastodon/bob"],"properties":{"http://webfinger.example/ns/name":"Bob Smith"}}`
+
+		if body != want {
+			t.Fatalf("got: %+v,\n want: %+v", body, want)
+		}
+	})
+
 	t.Run("nonexistent resources return 404", func(t *testing.T) {
 		req, _ := http.NewRequest(http.MethodGet, "/", nil)
 		query := req.URL.Query()
