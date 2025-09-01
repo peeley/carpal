76df67536c09b786f187b5472aeda5c8479639b3
Oliver Papst <oliver.papst@mailbox.org>
2025-09-01 19:00:59 +0200

    Add ldap.bind_pass_file to read ldap bind pass from file

--- internal/config/main.go
+++ internal/config/main.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
@@ -9,8 +10,11 @@ import (
 
 type ConfigWizard interface {
 	readConfigFile() ([]byte, error)
+	readSecretFromFile(filePath string) (string, error)
 	deserializeConfigYaml([]byte) (*Configuration, error)
+	processConfigYaml([]byte) (*Configuration, error)
 	GetConfiguration() (*Configuration, error)
+	processLDAPBindPassword(config *Configuration) error
 }
 
 type configWizard struct {
@@ -26,14 +30,15 @@ type FileConfiguration struct {
 }
 
 type LDAPConfiguration struct {
-	URL        string   `yaml:"url"`
-	BindUser   string   `yaml:"bind_user"`
-	BindPass   string   `yaml:"bind_pass"`
-	BaseDN     string   `yaml:"basedn"`
-	Filter     string   `yaml:"filter"`
-	UserAttr   string   `yaml:"user_attr"`
-	Attributes []string `yaml:"attributes"`
-	Template   string   `yaml:"template"`
+	URL          string   `yaml:"url"`
+	BindUser     string   `yaml:"bind_user"`
+	BindPass     string   `yaml:"bind_pass"`
+	BindPassFile string   `yaml:"bind_pass_file"`
+	BaseDN       string   `yaml:"basedn"`
+	Filter       string   `yaml:"filter"`
+	UserAttr     string   `yaml:"user_attr"`
+	Attributes   []string `yaml:"attributes"`
+	Template     string   `yaml:"template"`
 }
 
 type DatabaseConfiguration struct {
@@ -73,15 +78,91 @@ func (wiz configWizard) deserializeConfigYaml(
 	return &config, nil
 }
 
+func (wiz configWizard) processConfigYaml(
+	configYaml []byte,
+) (*Configuration, error) {
+	config, err := wiz.deserializeConfigYaml(configYaml)
+	if err != nil {
+		return nil, fmt.Errorf("cannot unmarshal config YAML: %w", err)
+	}
+
+	if err := wiz.processLDAPBindPassword(config); err != nil {
+		return nil, err
+	}
+
+	if err := wiz.processDatabaseURL(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func (wiz configWizard) readSecretFromFile(filePath string) (string, error) {
+	secretBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", fmt.Errorf("unable to read secret from %s: %w", filePath, err)
+	}
+
+	return string(bytes.TrimSpace(secretBytes)), nil
+}
+
+func (wiz configWizard) processLDAPBindPassword(config *Configuration) error {
+	if config.LDAPConfiguration == nil {
+		return nil
+	}
+
+	hasBindPass := config.LDAPConfiguration.BindPass != ""
+	hasBindPassFile := config.LDAPConfiguration.BindPassFile != ""
+
+	if hasBindPass == hasBindPassFile {
+		return fmt.Errorf("must specify either bind_pass or bind_pass_file")
+	}
+
+	if config.LDAPConfiguration.BindPassFile != "" {
+		password, err := wiz.readSecretFromFile(config.LDAPConfiguration.BindPassFile)
+		if err != nil {
+			return fmt.Errorf("cannot read LDAP bind password file: %w", err)
+		}
+
+		config.LDAPConfiguration.BindPass = password
+	}
+
+	return nil
+}
+
+func (wiz configWizard) processDatabaseURL(config *Configuration) error {
+	if config.DatabaseConfiguration == nil {
+		return nil
+	}
+
+	hasURL := config.DatabaseConfiguration.URL != ""
+	hasURLFile := config.DatabaseConfiguration.URLFile != ""
+
+	if hasURL == hasURLFile {
+		return fmt.Errorf("must specify either url or url_file")
+	}
+
+	if config.DatabaseConfiguration.URLFile != "" {
+		url, err := wiz.readSecretFromFile(config.DatabaseConfiguration.URLFile)
+		if err != nil {
+			return fmt.Errorf("cannot read database URL file: %w", err)
+		}
+
+		config.DatabaseConfiguration.URL = url
+	}
+
+	return nil
+}
+
 func (wiz configWizard) GetConfiguration() (*Configuration, error) {
 	configYaml, err := wiz.readConfigFile()
 	if err != nil {
 		return nil, fmt.Errorf("cannot read config file: %w", err)
 	}
 
-	config, err := wiz.deserializeConfigYaml(configYaml)
+	config, err := wiz.processConfigYaml(configYaml)
 	if err != nil {
-		return nil, fmt.Errorf("cannot unmarshal config YAML: %w", err)
+		return nil, err
 	}
 
 	return config, nil
